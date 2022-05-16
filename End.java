2d7ae35fa63ef6ad10d8d57a4b17a4225b7d72ec
lyssis147 <103008183+lyssis147@users.noreply.github.com>
2022-05-16 19:52:09 -0400

    Update End.java

--- End.java
+++ End.java
@@ -2,6 +2,7 @@ package project;
 
 public class End {
     public static void end(){
-        System.out.println("Goodbye!");
+        final String BYE ="Goodbye!";
+        System.out.println(BYE);
     }
 }
